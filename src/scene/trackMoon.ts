8f977c2d67975a405085b31ba00d2ed5cc9f37e8
CodedBeats <luca.haar@icloud.com>
2026-03-07 18:01:39 +1100

    feat: function to handle getting and setting moon pos (currently hard coded)

---
+++ src/scene/trackMoon.ts
@@ -0,0 +1,10 @@
+import * as THREE from "three";
+
+export function trackMoon(moonMesh: THREE.Mesh) {
+    // fetch moon location with api
+
+    // calculate relative postion to center of globe
+    
+    // move mesh to that location
+    moonMesh.position.set(0, 0, -70);
+}
