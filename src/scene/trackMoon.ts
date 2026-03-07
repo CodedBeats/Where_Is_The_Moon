bff6b9991d58506ec2bcb6dd919b179fe3c29654
CodedBeats <luca.haar@icloud.com>
2026-03-07 19:05:03 +1100

    feat: implemented seed data into track moon func

--- src/scene/trackMoon.ts
+++ src/scene/trackMoon.ts
@@ -1,13 +1,24 @@
 import * as THREE from "three";
+// helper func
+import { moonToXYZ } from "../utility/helpers";
+// temp seed data
+import { tempMoonData } from "../api/fetchMoonPosition";
 
 export function trackMoon(moonMesh: THREE.Mesh) {
     // fetch moon location with api
 
     // calculate relative postion to center of globe
-    
+    const { x, y, z} = moonToXYZ(
+        tempMoonData.data.moon.altitude,
+        tempMoonData.data.moon.azimuth,
+        tempMoonData.data.moon.distance,
+    )
+
     // move moon to that location
+    moonMesh.position.set(x, y, z);
+    
     // temp
-    moonMesh.position.set(0, 0, -10);
+    // moonMesh.position.set(0, 0, -10);
 
     // rotate moon to apropriate angle
     // temp
